33a9c49ab25e053c17e8d23580a2436291cb4329
Adam Yu <adamxhy123@gmail.com>
2019-07-20 20:15:46 -0400

    add logging to device sensors

--- m34/app/index.js
+++ m34/app/index.js
@@ -3,7 +3,7 @@ import { me } from 'appbit';
 import { Barometer } from 'barometer';
 import { BodyPresenceSensor } from 'body-presence';
 import { display } from 'display';
-import { openSync, writeSync } from 'fs';
+import { openSync, statSync, writeSync } from 'fs';
 import { geolocation } from 'geolocation';
 import { Gyroscope } from 'gyroscope';
 import { HeartRateSensor } from 'heart-rate';
@@ -11,26 +11,65 @@ import { OrientationSensor } from 'orientation';
 display.onchange = () => display.on = false;
 display.on = false;
 me.appTimeoutEnabled = false;
-const isAccelerometerOn = false;
-const isBarometerOn = false;
-const isBodyPresenceSensorOn = false;
-const isFsOn = false;
-const isGeolocationSensorOn = false;
-const isGyroscopeOn = false;
-const isHeartRateSensorOn = false;
-const isOrientationSensorOn = false;
-if (isAccelerometerOn) Accelerometer({ frequency: 1 });
-if (isBarometerOn) Barometer({ frequency: 1 });
-if (isBodyPresenceSensorOn) BodyPresenceSensor();
+const isAccelerometerOn = true;
+const isBarometerOn = true;
+const isBodyPresenceSensorOn = true;
+// filesystem
+const isFsOn = true;
+const isGeolocationSensorOn = true;
+const isGyroscopeOn = true;
+const isHeartRateSensorOn = true;
+const isOrientationSensorOn = true;
+if (isAccelerometerOn) {
+    const accelerometer = new Accelerometer({ batch: 1, frequency: 1 });
+    accelerometer.onreading = () => {
+        console.log(JSON.stringify(accelerometer.readings));
+    };
+    accelerometer.start();
+}
+if (isBarometerOn) {
+    const barometer = new Barometer({ batch: 1, frequency: 1 });
+    barometer.onreading = () => {
+        console.log(JSON.stringify(barometer.readings));
+    };
+    barometer.start();
+}
+if (isBodyPresenceSensorOn) {
+    const bodyPresenceSensor = new BodyPresenceSensor();
+    bodyPresenceSensor.onreading = () => {
+        console.log(JSON.stringify(bodyPresenceSensor.present));
+    };
+    bodyPresenceSensor.start();
+}
 if (isFsOn) {
     const file = openSync(me.applicationId, 'a+');
     setInterval(() => {
-        writeSync(file, Int8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]).buffer);
+        writeSync(file, (new Int8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).buffer);
+        console.log('Buffer written');
     }, 125);
 }
 if (isGeolocationSensorOn) setInterval(() => {
     geolocation.getCurrentPosition(_ => { });
+    console.log('Geolocation polled');
 }, 1000);
-if (isGyroscopeOn) Gyroscope({ frequency: 1 });
-if (isHeartRateSensorOn) HeartRateSensor({ frequency: 1 });
-if (isOrientationSensorOn) OrientationSensor({ frequency: 1 });
+if (isGyroscopeOn) {
+    const gyroscope = new Gyroscope({ batch: 1, frequency: 1 });
+    gyroscope.onreading = () => {
+        console.log(JSON.stringify(gyroscope.readings));
+    };
+    gyroscope.start();
+}
+if (isHeartRateSensorOn) {
+    const heartRateSensor = new HeartRateSensor({ batch: 1, frequency: 1 });
+    heartRateSensor.onreading = () => {
+        console.log(JSON.stringify(heartRateSensor.readings));
+    };
+    heartRateSensor.start();
+}
+if (isOrientationSensorOn) {
+    const orientationSensor = new OrientationSensor({ batch: 1, frequency: 1 });
+    orientationSensor.onreading = () => {
+        console.log(JSON.stringify(orientationSensor.readings));
+    };
+    orientationSensor.start();
+}
